a3495c7e88271347d623c7971e26eeb459929a34
James Percent <james@syndeticlog.org>
2012-10-25 00:52:29 -0400

    My fisher price Scala parse combinators

---
+++ src/main/scala/syndeticlogic/argot/sql/JSON.scala
@@ -0,0 +1,31 @@
+package syndeticlogic.argot.sql
+
+import scala.util.parsing.combinator._
+import java.io.FileReader
+
+class JSON extends JavaTokenParsers {
+  
+  def obj: Parser[Map[String, Any]] = 
+    "{"~> repsep(member, ",") <~"}" ^^ (Map() ++ _)
+  
+    def arr: Parser[List[Any]] = "["~> repsep(value, ",") <~"]"
+  
+  def member: Parser[(String,Any)] = stringLiteral~":"~value ^^ 
+      {case name~":"~value => (name, value)}
+    
+  def value: Parser[Any] =
+          obj | 
+          arr |
+          stringLiteral |
+          floatingPointNumber ^^ (_.toDouble) |
+          "null" ^^ (x => null) |
+          "true" ^^ (x => true) |
+          "false" ^^ (x => false) 
+}
+
+object ParseJSON extends JSON {
+    def main(args: Array[String]) {
+      val reader = new FileReader(args(0))
+      println(parseAll(value, reader))
+    }
+}

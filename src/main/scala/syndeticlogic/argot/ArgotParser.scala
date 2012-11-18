f9fd960569785c6e9ca2a56cd55653d821ee7968
James Percent <james@syndeticlog.org>
2012-11-18 13:09:38 -0500

    Refactored ArgotParser to support multiple statements.  Condensed DMLParserTest and DDLParserTest into ArgotParserTest

--- src/main/scala/syndeticlogic/argot/ArgotParser.scala
+++ src/main/scala/syndeticlogic/argot/ArgotParser.scala
@@ -10,23 +10,25 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 class ArgotParser extends JavaTokenParsers with DML with DDL {
-  def entryPoint: Parser[ArgotParseTree] = {
+  def argotStatement: Parser[ArgotParseTree] = {
     insertStmt |
     table |
     singleton
   }
+  
+  def entryPoint: Parser[List[ArgotParseTree]] = rep(argotStatement) ^^ (List() ++ _)
 }
 
 object ParseArgot extends ArgotParser {
   val logger: Log = LogFactory.getLog(this.getClass);
   
-  def parse(s: String): ArgotParseTree = {
+  def parse(s: String): List[ArgotParseTree] = {
     val reader = new StringReader(s)
     parse(reader)
   }
   
-  def parse(r: Reader): ArgotParseTree = {
-    val tree: ParseResult[ArgotParseTree] = parseAll(entryPoint, r)
+  def parse(r: Reader): List[ArgotParseTree] = {
+    val tree: ParseResult[List[ArgotParseTree]] = parseAll(entryPoint, r)
     logger.debug(tree)
     try { tree.get }
     catch { case e: RuntimeException => {
---
+++ src/test/scala/syndeticlogic/argot/ArgotParserTest.scala
@@ -0,0 +1,80 @@
+package syndeticlogic.argot
+
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import java.io.File
+import java.io.FileInputStream
+import java.io.ByteArrayOutputStream
+import scala.io.Source._
+
+class ArgotParserTest {
+  val table0 = "src/test/resources/test-files/table0.argot"
+  val table0Expected = "src/test/resources/test-files/table0Expected.argot"
+  val table1 = "src/test/resources/test-files/table1.argot"
+  val table1Expected = "src/test/resources/test-files/table1Expected.argot"
+  
+  val insert0 = "src/test/resources/test-files/insert0.argot"
+  val insert0Expected ="src/test/resources/test-files/insert0Expected.argot"
+  val insert1 ="src/test/resources/test-files/insert1.argot"
+  val insert1Expected = "src/test/resources/test-files/insert1Expected.argot"
+  val insert2 = "src/test/resources/test-files/insert2.argot"
+  val insert2Expected = "src/test/resources/test-files/insert2Expected.argot"
+    
+  def parseTest(inputQuery: String, queryBuilder: ArgotBuilder): String = {
+    var tree = ParseArgot.parse(inputQuery)
+    assertTrue(tree != null)
+    val file: List[String] = for(stmt <- tree) yield queryBuilder.build(stmt)
+    file.foldLeft("")((result, current) => result match {
+      case "" => current
+      case _ => result +"\n"+current
+    })
+  }
+    
+
+  @Test
+  def testCodeable(): Unit = {
+  }
+  
+  @Test
+  def testTable(): Unit = {
+    assertEquals(fromFile(table0Expected, "utf-8").getLines.mkString, parseTest(fromFile(table0, "utf-8").getLines.mkString, new DDLBuilder))
+    assertEquals(fromFile(table1Expected, "utf-8").getLines.mkString, parseTest(fromFile(table1, "utf-8").getLines.mkString, new DDLBuilder))
+  }
+
+  @Test
+  def testInsert(): Unit = {
+    assertEquals(fromFile(insert0Expected, "utf-8").getLines.mkString, parseTest(fromFile(insert0, "utf-8").getLines.mkString, new DMLBuilder))
+    assertEquals(fromFile(insert1Expected, "utf-8").getLines.mkString, parseTest(fromFile(insert1, "utf-8").getLines.mkString, new DMLBuilder))
+    
+    var exception = false
+    try {
+        assertEquals(fromFile(insert2Expected, "utf-8").getLines.mkString, parseTest(fromFile(insert2, "utf-8").getLines.mkString, new DMLBuilder))
+    } catch {
+      case e: RuntimeException => exception = true 
+    }
+    assertTrue(exception)
+  }
+  
+  // read a file an perseve end of line
+  def fileToString(file: File, encoding: String) = {
+    val inStream = new FileInputStream(file)
+    val outStream = new ByteArrayOutputStream
+    try {
+      var reading = true
+      while (reading) {
+        inStream.read() match {
+          case -1 => reading = false
+          case c => outStream.write(c)
+        }
+      }
+      outStream.flush()
+    } finally {
+      inStream.close()
+    }
+    new String(outStream.toByteArray(), encoding)
+  }
+}
